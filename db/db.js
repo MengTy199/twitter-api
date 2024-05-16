d63215987d48d0304cf378b8de640f384823d21b
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-05-16 11:07:35 +0700

    ch db

--- db/db.js
+++ db/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 require('dotenv').config();// env config
 
 // const url = "mongodb://localhost:2121/twitter-db"; for local
-const url = "mongodb+srv://mengty:V3FWrynq90iMpZsE@twitterdb.fwy18yf.mongodb.net/?retryWrites=true&w=majority"
-// const url = process.env.MOGODB_URL
+// const url = "mongodb+srv://mengty:V3FWrynq90iMpZsE@twitterdb.fwy18yf.mongodb.net/?retryWrites=true&w=majority"
+const url = process.env.MOGODB_URL
 
 async function dbConnect() {
   mongoose.connection.on("connected", () => console.log("Connected"));
