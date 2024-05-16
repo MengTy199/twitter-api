e70f93e860290cc396b820049319f2d0c008c2ad
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-05-16 10:57:35 +0700

    github

--- db/db.js
+++ db/db.js
@@ -8,7 +8,6 @@ const url = "mongodb+srv://mengty:V3FWrynq90iMpZsE@twitterdb.fwy18yf.mongodb.net
 async function dbConnect() {
   mongoose.connection.on("connected", () => console.log("Connected"));
   mongoose.connection.on("open", () => console.log("open"));
-  console.log(process.env.MOGODB_URL)
   await mongoose.connect(url,{
     dbName: 'tw-db'//create  db
   });
